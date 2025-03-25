810404b6116f84226402839599863e757503f94d
pantelis <pantelis.tsagkas@jet2.com>
2025-03-25 09:43:59 +0000

    delete test case 1

--- tests/test1.test.js
+++
@@ -1,8 +0,0 @@
-const request = require('supertest');
-const app = require('../app');
-
-test('GET / should return 200', async () => {
-    const response = await request(app).get('/');
-    expect(response.statusCode).toBe(200);
-    expect(response.text).toContain('Welcome to My Node.js App!');
-});
